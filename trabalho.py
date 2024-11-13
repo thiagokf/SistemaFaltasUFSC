315b95486b7f2ec5e38a49de446d2da6e29dce94
Thiago Fusco <165811535+thiagokf@users.noreply.github.com>
2024-11-13 20:05:18 -0300

    Update trabalho.py

--- trabalho.py
+++ trabalho.py
@@ -1,86 +1,118 @@
 #quantas aulas pode matar de poo
 
-class SistemaFaltas:
-    def __init__(self, creditosTotais, creditosFaltados):
-        self.creditosTotais = creditosTotais
-        self.creditosFaltados = creditosFaltados
-    
-    def numeroAulasFaltaveis(self):
-        return (self.creditosTotais * 0.25) - self.creditosFaltados
+class SistemaGeralUFSC:
+    def __init__(self):
+        self.creditosTotais = 102
+        self.alunos = []
         
-    def porcentagemMaximaPossivel(self):
-        return (self.creditosTotais - self.creditosFaltados) / self.creditosTotais
+    def acharMatricula(self, matricula):
+        achou = False
         
-class POO(SistemaFaltas):
-    def __init__(self, creditosFaltados, matricula):
-        super().__init__(102, creditosFaltados)
-        self.matricula = matricula
-        
-    def verificarAprovacaoPorPresenca(self):
-        if (super().porcentagemMaximaPossivel() < 0.75):
-            return False
-        else:
-            return True
-
-alunos = []
-
-def verificarSeJaExisteMatricula(matricula):
-    for aluno in alunos:
+        for aluno in self.alunos:
             if (aluno['matricula'] == matricula):
-                return True
+                return aluno
         
-    return False
+        if (achou == False):
+            print(f'Nenhum aluno foi encontrado com a matrícula {matricula}\n')
+            return None
+    
+    def numeroAulasFaltaveis(self, matricula):
+        aluno = self.acharMatricula(matricula)
+        aulasFaltaveis = (self.creditosTotais * 0.25) - int(aluno['numeroDeFaltas'])
+        
+        if (aulasFaltaveis < 0):
+            return 'Aluno já foi reprovado por frequência insuficiente'
+        
+        return aulasFaltaveis
+        
+    def porcentagemMaximaPossivel(self, matricula):
+        aluno = self.acharMatricula(matricula)
+        
+        return (self.creditosTotais - int(aluno['numeroDeFaltas'])) / self.creditosTotais
+
+class POO(SistemaGeralUFSC):
+    def verificarSeJaExisteMatricula(self, matricula):
+        for aluno in self.alunos:
+                if (aluno['matricula'] == matricula):
+                    return True      
+        return False
+        
+    def adicionarAluno(self, matricula, nome, numeroDeFaltas):
+        if (self.acharMatricula(matricula) != None):
+            print('Já existe um aluno com essa matrícula. Cada aluno deve ter uma matrícula única.')
+            return
+        
+        self.alunos.append({'nome': nome, 'matricula': matricula, 'numeroDeFaltas': numeroDeFaltas})
+        
+    def atualizarFichaAluno(self, matricula):
+        if (self.acharMatricula(matricula) == None):
+            print(f'Nenhum aluno foi encontrado com a matrícula {matricula}\n')
+            return
+        
+        print(f'Atualizando a ficha do aluno de matricula {matricula}')
+        nome = input('Digite o nome do aluno: ')
+        matricula = input('Digite a matricula do aluno: ')
+        numeroDeFaltas = int(input(f'Digite o número de faltas do aluno até hoje: '))
+        
+        aluno['nome'] = nome
+        aluno['matricula'] = matricula
+        aluno['numeroDeFaltas'] = numeroDeFaltas
+        achou = True
+        
+        print('Aluno adicionado')
+        
+    def deletarFichaAluno(self, matricula):
+        if (self.acharMatricula(matricula) == None):
+            print(f'Nenhum aluno foi encontrado com a matrícula {matricula}\n')
+            return
+        
+        index = alunos.index(aluno)
+        alunos.pop(index)
+        achou = True
+        print(f'Aluno de matrícula {matricula} foi deletado')
+    
+
+sistemaPOO = POO()
 
 while True:
     try:
-        opcao = int(input('1 - adicionar aluno \n2 - buscar aluno \n3 - atualizar ficha do aluno \n4 - deletar ficha de aluno \n5 - sair do programa \n'))
+        opcao = int(input('1 - adicionar aluno \n2 - buscar aluno \n3 - atualizar ficha do aluno \n4 - deletar ficha de aluno \n5 - Verificar porcentagem máxima possível \n6 - Verificar quantos dias o aluno ainda pode faltar para não reprovar \n7 - sair do programa \n'))
         
         if (opcao == 1):
             nome = input('Digite o nome do aluno: ')
+            numeroDeFaltas = input('Digite o número de faltas do aluno: ')
             matricula = input('Digite a matricula do aluno: ')
             
-            if (verificarSeJaExisteMatricula(matricula)):
-                return
-            
-            numeroDeFaltas = int(input(f'Digite o número de faltas do aluno de matrícula {matricula} até hoje: '))
-            alunos.append({'nome': nome, 'matricula': matricula, 'numeroDeFaltas': numeroDeFaltas})
+            sistemaPOO.adicionarAluno(matricula, nome, numeroDeFaltas)
             
         if (opcao == 2):
             matricula = input('Digite a matrícula do aluno que você quer buscar: \n')
-            achou = False
             
-            for aluno in alunos:
-                if (aluno['matricula'] == matricula):
-                    print(aluno)
-                    achou = True
-            
-            if (achou == False):
-                print(f'Nenhum aluno foi encontrado com a matrícula {matricula}\n')
+            print(sistemaPOO.acharMatricula(matricula))
         
         if (opcao == 3):
             matricula = input('Digite a matrícula do aluno que você quer atualizar a ficha: \n')
-            achou = False
             
-            for aluno in alunos:
-                if (aluno['matricula'] == matricula):
-                    print(f'Atualizando a ficha do aluno de matricula {matricula}')
-                    nome = input('Digite o nome do aluno: ')
-                    matricula = input('Digite a matricula do aluno: ')
-                    numeroDeFaltas = int(input(f'Digite o número de faltas do aluno até hoje: '))
-                    
-                    aluno['nome'] = nome
-                    aluno['matricula'] = matricula
-                    aluno['numeroDeFaltas'] = numeroDeFaltas
-                    
-                    achou = True
+            sistemaPOO.atualizarFichaAluno(matricula)
+                
+        if (opcao == 4):
+            matricula = input('Digite a matrícula do aluno que você quer deletar: \n')
             
-            if (achou == False):
-                print(f'Nenhum aluno foi encontrado com a matrícula {matricula}\n')
+            sistemaPOO.deletarFichaAluno(matricula)
+        
+        if (opcao == 5):
+            matricula = input('Digite a matrícula do aluno que você quer verificar: \n')
+            
+            print(f'{sistemaPOO.porcentagemMaximaPossivel(matricula)*100:.2f}%')
+            
+        if (opcao == 6):
+            matricula = input('Digite a matrícula do aluno que você quer verificar: \n')
+            
+            print(sistemaPOO.numeroAulasFaltaveis(matricula), ' dias')
+        
+        if (opcao == 7):
+            break
             
     except ValueError:
         print('Você deve digitar um número, não uma letra. Digite o numero 1, 2, 3, 4 ou 5')
 
-numeroFaltasPOO = 2
-
-situacaoPOO = POO(numeroFaltasPOO, '342342342')
-print(situacaoPOO.verificarAprovacaoPorPresenca())
